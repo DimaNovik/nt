01080b3800d8f7b286367cde62b9f3e5ecf09263
DimaNovik1989 <dimanovik1989@gmail.com>
2019-03-21 22:14:15 +0200

    Added popup. Fixed mobile.

--- dist/assets/js/common.js
+++ dist/assets/js/common.js
@@ -1,1 +1,22 @@
-alert('hello');
+window.addEventListener('load', function () {
+
+    (function togglePopup() {
+        var link = document.querySelector('.header__auth .link');
+        var popup = document.querySelector('.auth__popup');
+        var overlay = document.querySelector('.overlay');
+
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            if(!popup.classList.contains('active')) popup.classList.add('active');
+
+            if(!overlay.classList.contains('active')) overlay.classList.add('active');
+        });
+
+        overlay.addEventListener('click', function () {
+            popup.classList.remove('active');
+            this.classList.remove('active');
+        })
+    })();
+
+});
